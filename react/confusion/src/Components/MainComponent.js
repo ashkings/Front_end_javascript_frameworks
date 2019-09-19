01285796875e2ac239ce95f5b090fd2230a7cf39
Hunterx-Ashu <ashu.singla@hashedin.com>
2019-09-19 14:05:17 +0530

    React Redux Forms revisited

--- react/confusion/src/Components/MainComponent.js
+++ react/confusion/src/Components/MainComponent.js
@@ -9,25 +9,27 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
     comments: state.comments,
     promotions: state.promotions,
-    leaders: state.leaders
+    leaders: state.leaders,
   }
 }
 
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 
 class Main extends Component {
-  
+
   componentDidMount() {
     this.props.fetchDishes();
   }
@@ -66,7 +68,7 @@ class Main extends Component {
               <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
               <Redirect to="/home" />
           </Switch>
         </div>
--- react/confusion/src/redux/ActionCreators.js
+++ react/confusion/src/redux/ActionCreators.js
@@ -11,6 +11,7 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 });
 
+//Implementation of Thunk
 export const fetchDishes = () => (dispatch) => {
 
     dispatch(dishesLoading(true));
