5ed470e4cb7b2f416c19fd54e2b1877b559d085f
Hunterx-Ashu <ashu.singla@hashedin.com>
2019-09-27 11:40:52 +0530

    Task1 of Assignment4

--- react/confusion/src/Components/HomeComponent.js
+++ react/confusion/src/Components/HomeComponent.js
@@ -11,7 +11,7 @@ function RenderCard({item, isLoading, errMess}) {
                 <Loading />
         );
     }
-    else if (errMess) {
+    else if (errMess!==null) {
         return(
                 <h4>{errMess}</h4>
         );
@@ -43,7 +43,7 @@ function Home(props) {
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.dish}
                     isLoading={props.dishesLoading}
-                    errMess={props.dishesErrMess} />
+                    errMess={props.dishErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.promotion}
@@ -51,7 +51,9 @@ function Home(props) {
                     errMess={props.promoErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.leader} />
+                <RenderCard item={props.leader}
+                    isLoading={props.leaderLoading}
+                    errMess={props.leaderErrMess} />
                 </div>
             </div>
         </div>
